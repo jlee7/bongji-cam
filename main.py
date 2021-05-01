43eaca61024f4791f9fc6ca511a643a836d6d200
Josef <josef.lee@phobosys.de>
2021-05-01 15:49:21 +0200

    Change support from IR sensor to motion detection sensor

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ class BongjiCam:
     def __init__(self):
 
         # GPIO Pin
-        self.ObstaclePin = 11
+        self.ObstaclePin = 23
 
         # Variables
         self.remaining_time = 10
@@ -21,10 +21,10 @@ class BongjiCam:
         self.camera.set_resolution(1080, 720)
         self.camera.set_framerate(30)
         self.camera.set_annotations()
-     
+
         # IRObstacleDetection
         self.setup()
-    
+
     def start(self):
         # Threads
         self.thread_obstacle_loop = threading.Thread(target=self.loop, args=())
@@ -33,8 +33,8 @@ class BongjiCam:
         self.thread_obstacle_loop.start()
 
     def setup(self):
-        GPIO.setmode(GPIO.BOARD)       # Numbers GPIOs by physical location
-        GPIO.setup(self.ObstaclePin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.setmode(GPIO.BCM)       # Numbers GPIOs by physical location
+        GPIO.setup(self.ObstaclePin, GPIO.IN)
         logging.info("Setup finished.")
 
     def loop(self):
@@ -43,16 +43,16 @@ class BongjiCam:
             while True:
                 if (0 == GPIO.input(self.ObstaclePin)):
                     self.remaining_time = 10
-                    
+
                     if not self.is_recording:
                         self.camera.start_recording(time.strftime("bongji_cam_%Y-%m-%d_%H_%M_%S") + ".h264")
                         logging.info("Start recording.")
                         self.is_recording = True
-                    
+
         except KeyboardInterrupt:
             self.destroy()
-            logging.info("Stop IRObstacle loop.")       
-                
+            logging.info("Stop IRObstacle loop.")
+
     def destroy(self):
         GPIO.cleanup()
 
@@ -63,25 +63,19 @@ class BongjiCam:
                     logging.info("remaining_time is: " + str(self.remaining_time))
                     self.remaining_time -= 1
                     time.sleep(1)
-            
+
                 if self.is_recording:
                     self.camera.stop_recording()
                     logging.info("Stop recording.")
                     self.is_recording = False
-                    
+
         except KeyboardInterrupt:
-            logging.info("Stop countdown.")  
+            logging.info("Stop countdown.")
 
 if __name__ == '__main__':
     # Format log output
     format = "%(asctime)s: %(message)s"
     logging.basicConfig(format=format, level=logging.INFO, datefmt="%H:%M:%S")
-    
+
     bc = BongjiCam()
     bc.start()
-    
-    
-    
-    
-        
-    
